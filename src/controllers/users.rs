44e214896f5c823baee14b30d8e8eb2df0d22918
pontakornth <pontakorn61@gmail.com>
2020-04-21 11:24:32 +0700

    feat(account): Add ability to retrieve user profile

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -1,15 +1,33 @@
-use actix_web::{HttpResponse};
-use crate::models::users::User;
+use actix_web::{HttpResponse,web,Responder};
+use crate::models::users::{User,get_user_by_id};
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::Error;
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+pub struct IdPath {
+    pub id: i64
+}
 
-pub async fn get_userdata() -> HttpResponse {
-    HttpResponse::Ok()
-    .content_type("application/json")
-    .body(serde_json::to_string(
-        &User 
-        { username: String::from("most"),
-        display_name: String::from("Most")
-    }).unwrap())
+pub async fn get_userdata(db: web::Data<Pool<SqliteConnectionManager>>,param: web::Path<IdPath>) -> impl Responder {
+    let conn = db.get().unwrap();
+    match get_user_by_id(&conn, param.id) {
+        Ok(user) => {
+            HttpResponse::Found()
+                .content_type("application/json")
+                .body(serde_json::to_string(&user).unwrap())
+        },
+        Err(Error::QueryReturnedNoRows) => {
+            HttpResponse::NotFound()
+                .content_type("application/json")
+                .body("{status:404}")
+        },
+        Err(x) => {
+            HttpResponse::InternalServerError()
+                .body(format!("{}",x))
+        }
+    }
 }
 
 pub async fn change_password() -> HttpResponse {
--- src/models/users.rs
+++ src/models/users.rs
@@ -1,4 +1,9 @@
 use serde::{Serialize,Deserialize};
+use bcrypt::{hash,verify,DEFAULT_COST};
+use std::collections::HashMap;
+use r2d2::PooledConnection;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::Error;
 
 #[derive(Serialize,Deserialize)]
 pub struct User {
@@ -6,3 +11,56 @@ pub struct User {
    pub display_name: String
 }
 
+pub enum UserError {
+   UsernameAlreadyExist,
+   WrongPassword,
+   EmptyUsername,
+   UserDoesNotExist
+}
+
+fn generate_password(raw_password: &String) -> String {
+   hash(raw_password, DEFAULT_COST).unwrap()
+}
+
+fn verify_password(raw_password: &String,hash: &String) -> bool {
+   verify(raw_password, &hash).unwrap()
+}
+
+fn register(user_data: &HashMap<String,String>) -> Result<(),UserError> {
+   let mock_username = vec![String::from("mmknightx"),String::from("kenzie")];
+   let username = &user_data.get("username").unwrap();
+   if mock_username.contains(username) {
+      Err(UserError::UsernameAlreadyExist)
+   } else if username.len() == 0 {
+      Err(UserError::EmptyUsername)
+   } else {
+      Ok(())
+   }
+}
+
+pub fn get_user_by_username(db: &PooledConnection<SqliteConnectionManager>, username: &String) -> Result<User,Error> {
+   db.query_row(
+      "SELECT username, display_name FROM users WHERE username=?1", &[&username], |row| {
+         Ok(
+             User {
+                username: row.get::<_,String>(0).unwrap(),
+                display_name: row.get::<_,String>(1).unwrap()
+            }
+         )
+      }
+   )
+}
+
+pub fn get_user_by_id(db: &PooledConnection<SqliteConnectionManager>, id: i64) -> Result<User,Error> {
+   db.query_row(
+      "SELECT username, display_name FROM users WHERE id=?1", &[id], |row| {
+         println!("Id is {}",&id);
+         Ok(
+             User {
+                username: row.get::<_,String>(0).unwrap(),
+                display_name: row.get::<_,String>(1).unwrap()
+           }
+         )
+      }
+   )
+}
